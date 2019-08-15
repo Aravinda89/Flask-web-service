0d31a1d37a8b946072f920031f5bf4b67b25736e
aravindagayan <1513223865@qq.com>
2019-08-16 07:03:29 +0800

    html

--- hello_app.py
+++ hello_app.py
@@ -1,15 +1,16 @@
 from flask import request
 from flask import jsonify
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
 
 
 @app.route('/hello', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type', 'Authorization'])
 def hello():
     message = request.get_json(force=True)
     name = message['name']
-    response = {
-        'greeting':'Hello,'+name+'!'
-    }
+    response = {'greeting': 'Hello, ' + name + '!'}
     return jsonify(response)
